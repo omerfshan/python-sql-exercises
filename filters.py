eac9ffdcfc1934087288de09fbbcdcc78e8b4c05
sahan <101819553+omerfshan@users.noreply.github.com>
2024-09-22 00:45:49 +0300

    Update filters.py

--- filters.py
+++ filters.py
@@ -9,7 +9,7 @@ def getProduct(id):
     cursor.execute(sql,params)
     result= cursor.fetchone()
     print(f"id: {result[0]} name: {result[1]} price: {result[2]}")
-  
+    connection.close()
    
 
 getProduct(3)
